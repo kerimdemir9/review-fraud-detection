508349b6e768b18382182db8c4aa891ea8699a7a
Kerim Demir <kerim.demir2002@hotmail.com>
2025-03-05 14:17:07 +0300

    Update KafkaConfig.java

--- reviewsAPI/src/main/java/com/reviews/reviewsapi/KafkaConfig.java
+++ reviewsAPI/src/main/java/com/reviews/reviewsapi/KafkaConfig.java
@@ -19,7 +19,7 @@ public class KafkaConfig {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class); // ✅ FIXED
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class); 
 
         return new DefaultKafkaProducerFactory<>(configProps);
     }
